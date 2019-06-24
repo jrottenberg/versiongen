6181376168b4c44aa04c43f93909fa171c8c3f78
Julien Rottenberg <julien.rottenberg@disney.com>
2019-06-23 20:07:29 -0700

    validate

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # versiongen
 Generate a version file for CI
+
+
+
+```
+export CI_COMMIT_SHA=01234567890
+export CI_PIPELINE_IID=0
+export BUILD_NUMBER=0
+export CI_PROJECT_PATH=local/local
+export CI_COMMIT_REF_NAME=n/a
+export CI_REPOSITORY_URL=git@git.local:local
+```
+
+-->
+
+```
+{
+    "buildNumber": "0",
+    "buildTag": "0",
+    "buildTime": "2019-06-24T03:02:51.128071+00:00",
+    "scmBranch": "n/a",
+    "scmCommit": "01234567890",
+    "scmUrl": "git@git.local:local",
+    "version": "0"
+}
+```
--- src/run.py
+++ src/run.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env python3
 
 
+import os
+import json
+from datetime import datetime, timezone
+now = datetime.now(timezone.utc).isoformat()
+
+for i, j in os.environ.items():
+    print("{}={}".format(i, j))
+
+
+version = {}
+filename = 'version.json'
+
+
+# {
+#   "buildNumber": "47",
+#   "buildTag": "jenkins-Dmvrbackend-Build-Master-47",
+#   "buildTime": "2018-10-18T20:55:02Z",
+#   "scmBranch": "origin/master",
+#   "scmCommit": "2059d816be6a863df08b8803245812e0ac7010eb",
+#   "scmUrl": "git@code.wds.io:redshift/metadata_read.git",
+#   "version": "47_master"
+# }
+
+version['buildNumber'] = os.environ['CI_PIPELINE_IID']
+version['buildTag'] = os.environ['CI_PIPELINE_IID']
+version['buildTime'] = now
+version['scmBranch'] = os.environ['CI_COMMIT_REF_NAME']
+version['scmCommit'] = os.environ['CI_COMMIT_SHA']
+version['scmUrl'] = os.environ['CI_REPOSITORY_URL']
+version['version'] = os.environ['CI_PIPELINE_IID']
+
+
+with open(filename, 'w') as f:
+    json.dump(version, f)
